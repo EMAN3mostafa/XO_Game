8726e6b9a09bdc0a0de78431851ca18dafa9c4f8
Eman Mostafa <em630252@gmail.com>
2025-07-26 22:27:25 +0300

    first screen third update

--- lib/widgets/x0container_widget.dart
+++ lib/widgets/x0container_widget.dart
@@ -8,18 +8,28 @@ class XoContainerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 200,
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Image.asset(
-          imagePath,
-          fit: BoxFit.contain,
+    return Material( // ده اللي بيخلي الـ InkWell يشتغل
+      color: Colors.transparent, // عشان ميغيرش شكل الكونتينر
+      borderRadius: BorderRadius.circular(32), // نفس البوردر بتاع الكونتينر
+      child: InkWell(
+        onTap: () {
+          print('Container tapped!');
+        },
+        borderRadius: BorderRadius.circular(32), // لازم نكرر البوردر هنا كمان
+        child: Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(32),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.contain,
+            ),
+          ),
         ),
       ),
     );
